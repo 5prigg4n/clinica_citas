53ad923e73cabdfb80be2a6eec401e30100ea782
carlos <cordobacastro830@gmail.com>
2026-02-09 22:36:26 -0500

    Se crea estructura inicial del proyecto y se configura conexion con base de datos

---
+++ config/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database{
+
+private $host ;
+private $db_name;
+private $username;
+private $password;
+private $port;
+
+public function __construct(){
+
+    $this->host = 'localhost';
+    $this->db_name= 'clinica_citas';
+    $this->username= 'postgres';
+    $this->password= 'postgres';
+    $this->port= '5432';
+
+
+}
+public function connect(){
+    try{
+        $conn=new PDO(
+            "pgsql:host=".$this->host.";port=".$this->port.";dbname=".$this->db_name,
+            $this->username,
+            $this->password
+
+        );
+
+        return $conn;
+    }catch (PDOException $e){
+        die(" Error al conectarse: ".$e->getMessage());
+        }
+    }
+
+}
